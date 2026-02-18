41cf699b84fb1573d2fc0dd06cb6017a056d7af0
Mike <143658803+MichaelVera88@users.noreply.github.com>
2026-02-18 18:47:06 -0500

    Designed a function to train a given model with linear regression in trainer.py and save them to models.pkl

---
+++ src/trainer.py
@@ -0,0 +1,27 @@
+import joblib
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+
+def train_lr_model(preprocessor, x_train_dataset, y_train_dataset):
+    '''
+    Uses Linear Regression to train model from given data
+    
+    :param preprocessor: Preprocessor for Data
+    :param x_train_dataset: x Dataset
+    :param y_train_dataset: y Dataset
+    '''
+
+    lr_model = LinearRegression()
+    model_pipeline = Pipeline(steps=[("preprocessor", preprocessor), ("model", lr_model)])
+    model_pipeline.fit(x_train_dataset, y_train_dataset)
+
+    return model_pipeline
+
+def save_model(model):
+    '''
+    Saves trained model to model.pkl file
+    
+    :param model: Trained Model
+    '''
+    path = "models/model.pkl"
+    joblib.dump(model, path)
